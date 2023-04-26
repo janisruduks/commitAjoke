9022b138bb0cd474e919d24934a0842bea681955
Erkyill <janis.ruduks157@gmail.com>
2023-04-27 01:16:29 +0300

    All I ever wanted was to fight for a lord I believed in. But the good lords are dead and the rest are monsters.

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ProcessBuilder pb = new ProcessBuilder("git", "commit", "-m", getData());
+        Process p = pb.start();
     }
 
     public static String getData() throws IOException {
